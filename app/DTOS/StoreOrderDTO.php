abb67e5ef78b92ab013304d46c90135d4a3d66b1
Henrique Curtis <curtishenrique10@gmail.com>
2026-01-29 19:56:16 -0300

    fix: tornando status da comanda não obrigatorio

--- app/DTOS/StoreOrderDTO.php
+++ app/DTOS/StoreOrderDTO.php
@@ -3,13 +3,14 @@
 namespace App\DTOS;
 
 use App\Interfaces\DTOInterface;
+use App\OrderStatus;
 
 class StoreOrderDTO implements DTOInterface
 {
     public function __construct(
         public readonly string $number,
         public readonly string $responsible_name,
-        public readonly string $status,
+        public readonly ?string $status = OrderStatus::Open->value,
         public readonly array $products,
     ) {}
 
@@ -18,7 +19,7 @@ class StoreOrderDTO implements DTOInterface
         return new self(
             number: data_get($data, 'number'),
             responsible_name: data_get($data, 'responsible_name'),
-            status: data_get($data, 'status', 'aberta'),
+            status: data_get($data, 'status', OrderStatus::Open->value),
             products: data_get($data, 'products', []),
         );
     }
