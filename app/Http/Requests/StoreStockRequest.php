99e2bc0c068aac1bc89a9f6c00735e3d91967fef
ViniciusJ <viniciusjose9@outlook.com>
2026-01-28 21:22:27 -0300

    FIX: tirando obrigatoriedade da descrição

--- app/Http/Requests/StoreStockRequest.php
+++ app/Http/Requests/StoreStockRequest.php
@@ -22,7 +22,7 @@ class StoreStockRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'description' => 'required|string|max:255',
+            'description' => 'nullable|string|max:255',
             'products' => 'required|array|min:1',
             'products.*.id' => 'required|exists:products,id',
             'products.*.quantity' => 'required|integer|min:1',
